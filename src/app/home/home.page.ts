12dcdd000dc6b9b606541bd9f13932b3599ca9b4
AncaiFabi <fabiane.ancai331@gmail.com>
2023-06-13 22:21:09 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public numero = '0';
+  public numeroAux = '0';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.numero = '0';
+    this.numeroAux = '0';
+  }
+
+  somar() {}
+
+  sub() {}
+
+  mult() {}
+
+  dividir() {}
+
+  clear() {
+    this.numero = '0';
+    this.numeroAux = '0';
+  }
+
+  negativo() {
+    if (this.numero !='0') {
+      this.numero = this.numero.includes('-')
+      ? this.numero.replace('-','')
+      : '-' + this.numero;
+    }
+  }
+
+  percent() {}
+
+  igual() {}
+
+  numeros(num: string) {
+    if (this.numero.startsWith('0')) {
+      this.numero = '';
+    }
+    if (this.numero.length <= 8) {
+      this.numero += num;
+    }
+  }
+
+  virgula() {
+    if (this.numero.length <= 8 && !this.numero.includes(',')) {
+      this.numero += ',';
+    }
+  }
+}
